9b87de693f0e5d7fe599facd6d41e6a64668ee72
Phathh <phathuuho@gmail.com>
2021-10-14 20:28:21 +0700

    new project configure

---
+++ GunShop/src/main/java/com/intern/gunshop/GunshopApplication.java
@@ -0,0 +1,13 @@
+package com.intern.gunshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GunshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GunshopApplication.class, args);
+	}
+
+}
---
+++ GunShop/src/test/java/com/intern/gunshop/repository/RoleRepositoryTest.java
@@ -0,0 +1,28 @@
+package com.intern.gunshop.repository;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.Rollback;
+
+import com.intern.gunshop.entity.Role;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = Replace.NONE)
+@Rollback(false)
+public class RoleRepositoryTest {
+	
+	@Autowired
+	private RoleRepository repo;
+	
+	@Test
+	public void testCreateRole() {
+		Role savedRole = repo.save(new Role("CEO"));
+		
+		assertThat(savedRole.getRole_id()).isGreaterThan(0);
+	}
+}
